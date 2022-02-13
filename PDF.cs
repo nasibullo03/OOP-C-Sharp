c06c96ca3cc1da718cb879e711ec405fd7555a8f
Nasibullo <admin@kemsu.ru>
2022-02-13 21:49:39 +0700

    Добавлены новые параметры (дата создания, дата изменения, размер и пароль) в класс PDF изменены конструкторы для них

--- PDF.cs
+++ PDF.cs
@@ -5,22 +5,24 @@ namespace OOP_in_C_
     class PDF : Information
     {
 
-       /* public string DateOfCreation { set; get; }
-        public string DateOfChange { set; get; }
-        public string Size { set; get; }
+        public string DateOfCreation { get; set; }
+        public string DateOfChange { get; set; }
+        public string Size { get; set; }
+        public string Password { get; set; }
 
         public PDF() { }
-        public PDF(string name, string author, string[] keywords, string theme, string path, string DateOfCreation, string DateOfChange, string Size)
+        public PDF(string name, string author, string[] keywords, string theme, string path, string DateOfCreation, string DateOfChange, string Size, string Password)
+        : base(name, author, keywords, theme, path)
         {
             this.DateOfCreation = DateOfCreation;
             this.DateOfChange = DateOfChange;
             this.Size = Size;
-        }*/
+            this.Password = Password;
+        }
 
-
-
-
-
+        public PDF(string name, string author, string[] keywords, string theme, string path)
+        : base(name, author, keywords, theme, path) { }
+        public PDF(string name) : base(name) { }
 
     }
 }
